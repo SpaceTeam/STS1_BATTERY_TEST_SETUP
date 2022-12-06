b57eff31704cb7d7371d962118245fccba6b3ea8
Roland Strasser <rol1510@users.noreply.github.com>
2022-12-06 01:20:53 +0100

    test the transmission protocol on the board

--- software/board/src/main.rs
+++ software/board/src/main.rs
@@ -6,27 +6,43 @@
 use panic_rtt_target as _panic_handler;
 use rtic::app;
 
+use bbqueue::{BBBuffer, Consumer, Producer};
+use core::fmt::Write;
+use heapless::String;
+use rtt_target::rtt_init_print;
+use serde::{Deserialize, Serialize};
+use stm32f4xx_hal::block;
+use stm32f4xx_hal::serial::Event;
+use stm32f4xx_hal::{
+    gpio::{Output, Pin, PushPull},
+    pac,
+    prelude::*,
+    serial::*,
+};
+use systick_monotonic::{fugit::Duration, Systick};
+use transmission::{
+    receive::receive,
+    send::{send, setup},
+};
+
 #[app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [TIM2 ])]
 mod app {
-    use bbqueue::{BBBuffer, Consumer, Producer};
-    use core::fmt::Write;
-    use rtt_target::rtt_init_print;
-    use stm32f4xx_hal::block;
-    use stm32f4xx_hal::serial::Event;
-    use stm32f4xx_hal::{
-        gpio::{Output, Pin, PushPull},
-        pac,
-        prelude::*,
-        serial::*,
-    };
-    use systick_monotonic::{fugit::Duration, Systick};
+    use super::*;
 
-    static UART_BUFFER: BBBuffer<1024> = BBBuffer::new();
+    static UART_RX_BUFFER: BBBuffer<1024> = BBBuffer::new();
+    static UART_TX_BUFFER: BBBuffer<1024> = BBBuffer::new();
+
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    pub enum MsgTypes {
+        Msg(String<32>),
+        Test1(u32),
+        Test2(f32, u8),
+    }
 
     #[shared]
     struct Shared {
-        prod: Producer<'static, 1024>,
-        cons: Consumer<'static, 1024>,
+        prod_tx: Producer<'static, 1024>,
+        cons_rx: Consumer<'static, 1024>,
     }
 
     #[local]
@@ -34,6 +50,9 @@ mod app {
         led: Pin<'A', 5, Output<PushPull>>,
         rx: Rx<pac::USART2>,
         tx: Tx<pac::USART2>,
+
+        prod_rx: Producer<'static, 1024>,
+        cons_tx: Consumer<'static, 1024>,
     }
 
     #[monotonic(binds = SysTick, default = true)]
@@ -66,50 +85,54 @@ mod app {
         s.listen(Event::Rxne);
 
         let (mut tx, rx) = s.split();
-        let (prod, cons) = UART_BUFFER.try_split().unwrap();
-
-        write!(tx, "INIT!\r\n").unwrap();
+        let (prod_rx, cons_rx) = UART_RX_BUFFER.try_split().unwrap();
+        let (mut prod_tx, cons_tx) = UART_TX_BUFFER.try_split().unwrap();
 
         blink::spawn().ok();
 
+        setup(&mut prod_tx);
+        send(&mut prod_tx, MsgTypes::Msg(String::from("IT WORKS!!!")));
+
         (
-            Shared { prod, cons },
-            Local { led, rx, tx },
+            Shared { prod_tx, cons_rx },
+            Local {
+                led,
+                rx,
+                tx,
+                prod_rx,
+                cons_tx,
+            },
             init::Monotonics(mono),
         )
     }
 
-    #[task(local = [led, tx], shared =[cons], priority = 4)]
+    #[task(local = [led, tx, cons_tx], shared =[prod_tx], priority = 4)]
     fn blink(mut ctx: blink::Context) {
         ctx.local.led.toggle();
 
-        write!(ctx.local.tx, "Blink\r\n").unwrap();
+        let rgr = match ctx.local.cons_tx.read() {
+            Ok(rgr) => {
+                rgr.buf()
+                    .iter()
+                    .for_each(|&byte| block!(ctx.local.tx.write(byte)).unwrap());
 
-        ctx.shared.cons.lock(|cons| {
-            let rgr = match cons.read() {
-                Ok(it) => it,
-                _ => return,
-            };
-            rgr.buf()
-                .iter()
-                .for_each(|&byte| block!(ctx.local.tx.write(byte)).unwrap());
+                let len = rgr.len();
+                rgr.release(len);
+            }
+            _ => (),
+        };
 
-            let len = rgr.len();
-            rgr.release(len);
-        });
         blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1500)).ok();
     }
 
-    #[task(binds = USART2, local = [rx], shared =[prod])]
+    #[task(binds = USART2, local = [rx, prod_rx])]
     fn serial(mut ctx: serial::Context) {
         match block!(ctx.local.rx.read()) {
             Ok(byte) => {
-                ctx.shared.prod.lock(|prod| {
-                    if let Ok(mut wgr) = prod.grant_exact(1) {
-                        wgr[0] = byte;
-                        wgr.commit(1);
-                    }
-                });
+                if let Ok(mut wgr) = ctx.local.prod_rx.grant_exact(1) {
+                    wgr[0] = byte;
+                    wgr.commit(1);
+                }
             }
             Err(_) => {}
         }
--- software/client/src/main.rs
+++ software/client/src/main.rs
@@ -1,5 +1,19 @@
+use bbqueue::{BBBuffer, Consumer, Producer};
+use heapless::String;
+use serde::{Deserialize, Serialize};
 use serialport::{self, SerialPort};
 use std::{thread, time};
+use transmission::{
+    receive::receive,
+    send::{send, setup},
+};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum MsgTypes {
+    Msg(String<32>),
+    Test1(u32),
+    Test2(f32, u8),
+}
 
 fn main() {
     println!("Hello, world!");
@@ -9,13 +23,33 @@ fn main() {
         .expect("Couldn't open the serial port");
 
     println!("{:?}", port.name());
-    let count = port.bytes_to_read().unwrap();
 
-    print_input(&mut port);
+    let buf_tx: BBBuffer<1024> = BBBuffer::new();
+    let buf_rx: BBBuffer<1024> = BBBuffer::new();
+    let (mut prod_tx, mut cons_tx) = buf_tx.try_split().unwrap();
+    let (mut prod_rx, mut cons_rx) = buf_rx.try_split().unwrap();
 
-    write!(port, "Received {} bytes", count).unwrap();
-    thread::sleep(time::Duration::from_secs(1));
-    print_input(&mut port);
+    let count = port.bytes_to_read().unwrap() as usize;
+    let mut buf = vec![0u8; count as usize];
+    port.read(&mut buf);
+    dbg!(&buf);
+
+    let mut wgr = prod_rx.grant_exact(count).unwrap();
+    wgr.buf().copy_from_slice(buf.as_slice());
+    wgr.commit(count);
+
+    // dbg!(cons_rx.read().unwrap());
+
+    // receive(cons, cb)
+    receive::<MsgTypes, 1024>(&mut cons_rx, |msg| {
+        println!("{:?}", msg);
+    });
+
+    // print_input(&mut port);
+
+    // write!(port, "Received {} bytes", count).unwrap();
+    // thread::sleep(time::Duration::from_secs(1));
+    // print_input(&mut port);
 
     println!("Done");
 }
@@ -25,6 +59,6 @@ fn print_input(port: &mut Box<dyn SerialPort>) {
     let mut buf = vec![0u8; count as usize];
     port.read(&mut buf);
 
-    let text = String::from_utf8_lossy(&buf);
-    println!("{:?}", text);
+    // let text = String::from_utf8_lossy(&buf);
+    println!("{:?}", buf);
 }
